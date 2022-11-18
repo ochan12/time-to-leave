d91bbca58e62a52a70d9690c409f717df1d1fb0e
Mateo Ochandorena <mateochando@gmail.com>
2022-11-18 19:48:06 +0100

    Create notification channel test

--- js/notification-channel.js
+++ js/notification-channel.js
@@ -1,8 +1,15 @@
 const { ipcRenderer } = require('electron');
 
-// Event handler to search for #leave-by element, not accesible through main process
-ipcRenderer.on('GET_LEAVE_BY', (event) =>
+const searchLeaveByElement = (event) =>
 {
     const leaveByElement = $('#leave-by').val();
     event.sender.send('RECEIVE_LEAVE_BY', leaveByElement);
-});
+};
+
+// Event handler to search for #leave-by element, not accesible through main process
+ipcRenderer.on('GET_LEAVE_BY', searchLeaveByElement);
+
+
+module.exports = {
+    searchLeaveByElement
+};
