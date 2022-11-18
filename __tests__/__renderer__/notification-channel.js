9ecc9f15896c6cbab2537a569d1ec1aee744f5e0
Mateo Ochandorena <mateochando@gmail.com>
2022-11-18 19:48:06 +0100

    Create notification channel test

---
+++ __tests__/__renderer__/notification-channel.js
@@ -0,0 +1,22 @@
+const notificationChannel = require('../../js/notification-channel');
+
+describe('Notifications channel',  () =>
+{
+
+    it('Should get content of #leave-by element', done =>
+    {
+        window.$ = require('jquery');
+        $('body').append('<input id="leave-by" value="12:12" />');
+        // Way to get the file considered for coverage
+        notificationChannel.searchLeaveByElement({
+            sender: {
+                send: (channel, value) =>
+                {
+                    expect(channel).toBe('RECEIVE_LEAVE_BY');
+                    expect(value).toBe('12:12');
+                    done();
+                }
+            }
+        });
+    });
+});
